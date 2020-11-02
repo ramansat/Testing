19c7eee0ae789ab3e512336dde5587c6fb9bcdbe
Raman Sathiapalan <raman@ramans-air.home>
2020-11-02 15:19:47 -0600

    Commiting main

--- src/TestingJavaGit.java
+++ src/TestingJavaGit.java
@@ -1,5 +1,6 @@
 public class TestingJavaGit {
     public static void main(String args[]) {
-	System.out.println("Hello");         
+	System.out.println("Hello"); 
+	System.out.println("Hello again");         
     }
 }
