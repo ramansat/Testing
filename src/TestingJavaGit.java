01588bb99fda41ab07dd2da7607b4bff8e98a578
Raman Sathiapalan <raman@ramans-air.home>
2020-11-02 15:11:23 -0600

    Second commit

--- src/TestingJavaGit.java
+++ src/TestingJavaGit.java
@@ -1,5 +1,7 @@
 public class TestingJavaGit {
     public static void main(String args[]) {
-	System.out.println("Hello");         
+	System.out.println("Hello"); 
+	System.out.println("Second edit");
+	System.out.println("Testing second branch");          
     }
 }
